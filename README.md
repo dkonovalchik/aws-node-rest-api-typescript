7512b234ca38e0984b63453f73da655f9b849d47
Dyukon <dyukon@gmail.com>
2023-08-17 11:48:09 +0600

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+Serverless Nodejs Rest API with TypeScript And MongoDB Atlas
+https://www.serverless.com/examples/aws-node-rest-api-typescript
+
+Guthub:
+https://github.com/serverless/examples/tree/master/aws-node-rest-api-typescript
---
+++ app/handler.ts
@@ -0,0 +1,35 @@
+import { Handler, Context } from 'aws-lambda';
+
+import dotenv from 'dotenv';
+import path from 'path';
+const dotenvPath = path.join(__dirname, '../../', '.env');
+dotenv.config({
+  path: dotenvPath,
+});
+
+import { books } from './model';
+import { BooksController } from './controller/books';
+
+console.log('process.env:', process.env);
+
+const booksController = new BooksController(books);
+
+export const create: Handler = (event: any, context: Context) => {
+  return booksController.create(event, context);
+};
+
+export const update: Handler = (event: any, context: Context) => {
+  return booksController.update(event, context);
+}
+
+export const find: Handler = (event: any, context: Context) => {
+  return booksController.find(event, context);
+}
+
+export const findOne: Handler = (event: any, context: Context) => {
+  return booksController.findOne(event, context);
+};
+
+export const deleteOne: Handler = (event: any, context: Context) => {
+  return booksController.deleteOne(event, context);
+}
